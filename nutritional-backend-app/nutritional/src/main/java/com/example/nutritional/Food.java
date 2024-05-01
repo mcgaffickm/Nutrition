778d6b7e68deb6acce177074438d3532f44811fc
mcgaffickm <michael.mcgaffick@gmail.com>
2024-05-01 18:28:49 -0400

    Adds templates for spring to communicate with react

---
+++ nutritional-backend-app/nutritional/src/main/java/com/example/nutritional/Food.java
@@ -0,0 +1,16 @@
+package com.example.nutritional;
+
+//@Entity
+public class Food {
+
+  //  @Id
+    //@GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String date;
+    private int calories;
+    private int fat;
+    private int protien;
+    private int carbs;
+    
+}
---
+++ nutritional-frontend-app/src/AddFood.js
@@ -0,0 +1,54 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+
+function AddFood(){
+  const [food, setFood] = useState({ name: '', date: '', calories: '', fat: '' ,protien: '' ,carbs: ''   });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('<http://localhost:8080/food>', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(food),
+    })
+    .then(response => response.json())
+    .then(data => console.log('Food created:', data))
+    .catch(error => console.error('Error creating food:', error));
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFood(prevFood => ({ ...prevFood, [name]: value }));
+  };
+    //Creates the input form for the food entries
+    return(
+      <div>
+        <h1 align="center">Add a food</h1>
+        <form align="center" onSubmit={handleSubmit}>
+            <label>Name of Food: </label>
+            <input name= "name" type = "text" value={food.name} onChange={handleChange}></input>
+            <br /><br />
+            <label>Date Eaten: </label>
+            <input name= "date" type = "date" value={food.date} onChange={handleChange}></input>
+            <br /><br />
+            <label>Calories: </label>
+            <input name= "calories" type = "text" value={food.calories} onChange={handleChange}></input>
+            <br /><br />
+            <label>Fat:</label>
+            <input name= "fat" type = "text" value={food.fat} onChange={handleChange}></input>
+            <br /><br />
+            <label>Protien: </label>
+            <input name= "protien" type = "text" value={food.protien} onChange={handleChange}></input>
+            <br /><br />
+            <label>Carbohydrates: </label>
+            <input name= "carbs" type = "text" value={food.carbs} onChange={handleChange}></input>
+            <br /><br />
+            <button name="submit" type="submit">Submit</button>
+        </form>
+      </div>
+    )
+  }
+
+  export default AddFood;

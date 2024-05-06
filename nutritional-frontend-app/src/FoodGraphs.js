ec2bd09bd81cc64b42cfbe9ffcd622c91e88575f
mcgaffickm <michael.mcgaffick@gmail.com>
2024-05-05 23:20:21 -0400

    Cleans up add food user interface

--- nutritional-frontend-app/src/FoodGraphs.js
+++ nutritional-frontend-app/src/FoodGraphs.js
@@ -1,4 +1,16 @@
+import React, { useEffect, useState } from 'react';
+
 function FoodGraphs(){
+  const [foods, setFoods] = useState([]);
+
+  //Post request to get all the food data
+  useEffect(() => {
+    fetch('http://localhost:8080/foods')
+      .then(response => response.json())
+      .then(data => setFoods(data))
+      .catch(error => console.error('Error fetching food data:', error));
+  }, []);
+
     return(
       <div>
         <h2>A Food Graph will go here</h2>
